8ac1e3ba524f7ddce9002af6595fc8051a46add2
Tresor <58241586+tresor98@users.noreply.github.com>
2021-07-20 09:55:56 +0800

    Add files via upload

---
+++ palmares.py
@@ -0,0 +1,14 @@
+import streamlit as st 
+import pandas as pd 
+
+
+st.write("""
+# Lycee du Saint Esprit finalistes depuis 1958
+	""")
+
+selected_year = st.sidebar.selectbox('Year',list((range(1958,2020)))) 
+
+data = pd.read_excel('D:/Admin/Desktop/Palmares.xlsx')
+
+st.write(data[data["Date d'adieu"].dt.year== int(selected_year)])
+st.write(selected_year)
